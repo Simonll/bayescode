1658a4e041310621b8dd2fd1f6c8ddfb8082fdbf
sll <evol.simon@gmail.com>
2026-05-05 17:03:24 -0400

    removed bl multiplicator

--- src/ReadCodonMutSelMultipleOmega.cpp
+++ src/ReadCodonMutSelMultipleOmega.cpp
@@ -174,7 +174,7 @@ int main(int argc, char *argv[]) {
             for (Tree::NodeIndex node = 0; node < Tree::NodeIndex(model.GetTree().nb_nodes());
                  node++) {
                 if (!model.GetTree().is_root(node)) {
-                    export_tree.set_tag(node, "length", to_string(model.GetBranchLength(node) * 3));
+                    export_tree.set_tag(node, "length", to_string(model.GetBranchLength(node)));
                 }
             }
             os << export_tree.as_string() << '\n';
